bbde9ebe27e36ef132ab16be2597a086b962062c
Artem Petrushenko <petrusav@fuib.com>
2025-05-13 15:24:37 +0300

    flutter splash screens and native app

---
+++ flutter/splash_screen/nativeSplashScreenIOS/nativeSplashScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nativeSplashScreen
+//
+//  Created by Artem Petrushenko on 23.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ flutter/splash_screen/nativeSplashScreenIOS/nativeSplashScreen/nativeSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  nativeSplashScreenApp.swift
+//  nativeSplashScreen
+//
+//  Created by Artem Petrushenko on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct nativeSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
